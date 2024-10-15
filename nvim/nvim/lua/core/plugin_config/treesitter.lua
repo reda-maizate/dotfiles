d8296018ec2723661ace100cf3b926b68f8bf95b
navyblueglove <rmaizate@scaleway.com>
2024-10-15 14:51:53 +0200

    nvim(treesitter): add tmux command binding to nvim

--- nvim/nvim/lua/core/plugin_config/treesitter.lua
+++ nvim/nvim/lua/core/plugin_config/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "c", "lua", "rust", "python", "vim", "html", "cpp", "cuda", "go", "zig" },
+  ensure_installed = { "c", "lua", "rust", "python", "vim", "html", "cpp", "cuda", "go", "zig", "dockerfile", "dot"},
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
